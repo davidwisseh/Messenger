0271594843cfb85e8e833982cad39baa22d75c43
davidwisseh <{ID}+{username}@users.noreply.github.com>
2024-07-11 18:40:46 -0400

    fix 22

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -96,7 +96,7 @@ export async function POST(req: Request) {
     console.log("getting user from firestore");
     const docSnap = await db.collection("Users").doc(userId);
     if (docSnap) {
-      console.log("user exists in database : ");
+      console.log("user exists in database : " + JSON.stringify(docSnap));
     } else {
       console.log("creating user in database");
       db.collection("Users").add({ userId: { "Signed in": true } });
