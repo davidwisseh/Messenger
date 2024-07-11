1a99678574f2ad8895c3ef0531df7610023effa4
Davidwisseh <15909768+davidwisseh@users.noreply.github.com>
2024-07-11 11:38:32 -0400

    fix 1

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -2,6 +2,8 @@
 import { headers } from "next/headers";
 
 export async function POST(req: Request) {
+  console.log("reqested");
   const res = await req.json();
   console.log(req);
+  return req;
 }
