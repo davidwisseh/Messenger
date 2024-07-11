9995e947688ddb7a9e20be0db34dd9cc65b54209
Davidwisseh <15909768+davidwisseh@users.noreply.github.com>
2024-07-11 11:27:22 -0400

    fix 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,29 @@
-"use server";
 import Image from "next/image";
 import Navbar from "../components/NavBar";
-import { app } from "@/app/layout";
 import { useEffect } from "react";
 import { getAuth, signInWithCustomToken } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { doc, getDoc } from "firebase/firestore";
 import { auth as useAuth } from "@clerk/nextjs/server";
-
-const db = getFirestore(app);
-// Connect to Firebase auth
-const auth = getAuth(app);
+import { initializeApp } from "firebase/app";
 
 export default async function Home() {
+  const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: "messenger-fdf1b.firebaseapp.com",
+    projectId: "messenger-fdf1b",
+    storageBucket: "messenger-fdf1b.appspot.com",
+    messagingSenderId: "70071304038",
+    appId: "1:70071304038:web:137a06f0b6f96b8dae92a7",
+    measurementId: "G-BGRXQL8988",
+  };
+
+  const app = initializeApp(firebaseConfig);
+
+  const db = getFirestore(app);
+  // Connect to Firebase auth
+  const auth = getAuth(app);
+
   return (
     <>
       <Navbar></Navbar>
