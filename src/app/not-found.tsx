41609492788b7047268f7ad70e59c78e4b5abf4f
davidwisseh <{ID}+{username}@users.noreply.github.com>
2024-07-17 17:11:38 -0400

    11pmam

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,7 +2,7 @@
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
-export default function notFound() {
+export default function NotFound() {
   const router = useRouter();
   useEffect(() => {
     router.push("/");
