aae770330592a9ad6631480468199586d228672f
davidwisseh <{ID}+{username}@users.noreply.github.com>
2024-07-18 20:51:11 -0400

    11pmam

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,7 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
+  maximumScale: 1,
 };
 
 export default function RootLayout({
